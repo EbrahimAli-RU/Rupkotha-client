bc907afd0790e425a539d305e0fd1df61150f339
Ebrahim <ebrahimali.cse.ru@gmail.com>
2021-06-05 08:01:35 +0600

    design dropdown and home page carosul

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { Switch, Route } from 'react-router-dom'
 import ForgotPassword from './container/auth/ForgotPassword';
 import Signin from './container/auth/Signin';
 import Signup from './container/auth/Signup';
-import Navigation from './layout/Navigation';
+import Main from './container/home/Main'
 
 import './sass/main.scss'
 function App() {
   return (
     <Switch>
-      <Route path='/home' component={Navigation} />
+      <Route path='/home' component={Main} />
       <Route path='/register' component={Signup} />
       <Route path='/forgot-password' component={ForgotPassword} />
       <Route path='/' component={Signin} />
---
+++ src/container/home/Main.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navigation from '../../layout/Navigation'
+import Icon from '../../assets/img/sprite.svg'
+import Book from '../../assets/img/little.png'
+
+const main = () => {
+    return (
+
+        <div className='main__carosoul'>
+            <Navigation />
+            <div className='main__carosoul__content'>
+                <div className='main__carosoul__left__div'>
+                    <h1>Little Socks Makes a Friend</h1>
+                    <div>
+                        <button className='btn btn-read'> 
+                        <svg className="nav__items__icon" style={{marginRight: '1rem', fill: 'white'}}>
+                                <use xlinkHref={`${Icon}#icon-home`}></use>
+                            </svg>Read</button>
+                        <div className="main__carosoul__wishlist" >
+                            <svg className="nav__items__icon">
+                                <use xlinkHref={`${Icon}#icon-home`}></use>
+                            </svg>
+                        </div>
+                    </div>
+                    <p>Little Sock loves to visit Sock City—but sometimes, 
+                        he gets a bit lonely. Will he be able to find a new friend?</p>
+                        <p>4m 47s• New Releases• Popular Titles• Age: Elementary</p>
+                </div>
+                <img src={Book} alt='book' />
+            </div>
+        </div>
+    );
+};
+
+export default main;
