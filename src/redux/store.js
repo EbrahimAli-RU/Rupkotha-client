a26a45a241e25bddfc0f87c40cd35422298617cb
Ebrahim <ebrahimali.cse.ru@gmail.com>
2021-08-04 16:17:14 +0600

    forgot password works successfully

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,14 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 
 import profileReducer from './reducer/profile'
 import wishlistReducer from './reducer/wishlist'
+import uiReducer from './reducer/ui'
+import userReducer from './reducer/user'
+
 const rootReducer = combineReducers({
     profile: profileReducer,
-    wishlist: wishlistReducer
+    wishlist: wishlistReducer,
+    ui: uiReducer,
+    user: userReducer
 });
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
