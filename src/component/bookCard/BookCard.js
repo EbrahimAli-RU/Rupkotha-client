bfd73f724d7ec7e0a5a2bb0aca3e39acf39a0856
Ebrahim <ebrahimali.cse.ru@gmail.com>
2021-08-06 10:43:36 +0600

    change styles an create three pages

--- src/component/bookCard/BookCard.js
+++ src/component/bookCard/BookCard.js
@@ -9,12 +9,18 @@ const BookCard = (props) => {
     return (
         <>
             <div className={`bookCard__wrapper width${props.width}`} >
-            <a href={props.link}><img className='bookCard__wrapper__img' src={`http://localhost:8000/${props.book}`} alt='book' /></a>
-                <div className="main__carosoul__wishlist" style={{position: 'absolute', left: '2rem', bottom: '2rem'}} >
-                    {props.wishlist !== -1 ? <FontAwesomeIcon onClick={() => props.handler(props.id, props.category)} icon={fasHeart}  style={{color: 'red', fontSize: '2.5rem', cursor: 'pointer'}}  />:
-                    <FontAwesomeIcon onClick={() => props.handler(props.id, props.category)} icon={faHeart}  style={{color: 'red', fontSize: '2.5rem', cursor: 'pointer'}} />}
+                <a href={props.link}>
+                    <img className='bookCard__wrapper__img'
+                        src={`http://localhost:8000/${props.book}`} alt='book' />
+                </a>
+                <div className="main__carosoul__wishlist card__bw__position left">
+                    {props.wishlist !== -1 ?
+                        <FontAwesomeIcon onClick={() => props.handler(props.id, props.category)}
+                            icon={fasHeart} style={{ color: 'red', fontSize: '2.5rem', cursor: 'pointer' }} /> :
+                        <FontAwesomeIcon onClick={() => props.handler(props.id, props.category)}
+                            icon={faHeart} style={{ color: 'red', fontSize: '2.5rem', cursor: 'pointer' }} />}
                 </div>
-                <div className="main__carosoul__wishlist" style={{position: 'absolute', right: '2rem', bottom: '2rem'}} >
+                <div className="main__carosoul__wishlist card__bw__position right" >
                     <Link to='/read'>
                         <svg className="nav__items__icon">
                             <use xlinkHref={`${Icon}#icon-book`}></use>
@@ -23,7 +29,7 @@ const BookCard = (props) => {
                 </div>
             </div>
         </>
-        
+
     );
 };
 
